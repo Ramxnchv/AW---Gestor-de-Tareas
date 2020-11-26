3b47c4641a752091d891c2a4111e640c454987fd
Ramón <ramonchuwii@gmail.com>
2020-11-26 17:09:40 +0100

    npm init

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ module.exports = {
       host: "localhost",// Ordenador que ejecuta el SGBD
       user: "root",   // Usuario que accede a la BD
       password: "",  // Contraseña con la que se accede a la BD
-      database: "tareas"  // Nombre de la base de datos
+      database: "tasks"  // Nombre de la base de datos
 }
--- main.js
+++ main.js
@@ -18,3 +18,14 @@ let daoTask = new DAOTasks(pool);
 
 // Definición de las funciones callback
 // Uso de los métodos de las clases DAOUsers y DAOTasks
+daoUser.isUserCorrect("usuario@ucm.es", "mipass", cb_isUserCorrect);
+
+function cb_isUserCorrect(err, result){
+   if (err) {
+       console.log(err.message);
+   } else if (result) {
+       console.log("Usuario y contraseña correctos");
+   } else {
+       console.log("Usuario y/o contraseña incorrectos");
+   }
+}
